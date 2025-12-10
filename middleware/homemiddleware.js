e3a2efad6422442b4e2adf9252e91bbdc102a2d1
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-12-10 11:48:07 +0100

    no problem here

--- middleware/homemiddleware.js
+++ middleware/homemiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const homepagemiddlewaer = async (req,res,next) =>{
     try{    
-    const authtokin  = req.headers['authorization'];
+    const authtokin  = req.headers['authorization'];// this contain the bearer token that has our encrypted pay load 
 
 
     if(!authtokin){
@@ -14,20 +14,23 @@ const homepagemiddlewaer = async (req,res,next) =>{
         // verify the tokin 
         const acesstoken = authtokin.split(' ')[1]
         const verifyingtoken = await jwt.verify(acesstoken,process.env.jwtacesstoken)
+        // to verify the acess token we have to use the verify key word and pass it with our jwt service key 
 
         if(!verifyingtoken){
+            // this entire section check if the acess tokin providen matched with the one generated 
             res.satus(400).json({
                 message : 'invalid acess token '
             })
         }
         else{
+            // this section holds the acess token and store it in a request user infor in the middleware 
             req.userinformation = verifyingtoken
         }
         
     }
 
 
-    next()
+    next()// this next go to the next handler preceding the routhes 
     }
     catch(error){
         res.status(500).json({
--- routes/applicationrouthes.js
+++ routes/applicationrouthes.js
@@ -7,9 +7,9 @@ routers = express.Router()
 
 
 routers.post('/register',handlingregister)
-routers.post('/login',handlinglogin)
+routers.post('/login',handlinglogin)// when login is scuessful an acess token is cerated for each user to stay login so we always have  toverify the token that where the middle waere comes in place 
 routers.get('/user-dashboard',homepagemiddlewaer,handlinggoingtohome),
-routers.get('/admin',homepagemiddlewaer,adminmiddlewaer,handlingadmindashboard)
+routers.get('/admin',homepagemiddlewaer,adminmiddlewaer,handlingadmindashboard)//middle waer  checks the user role 
 // not we can protect routhes by using middle wear the routhes can take mutiple middle weare as handler 
 
 module.exports =routers
