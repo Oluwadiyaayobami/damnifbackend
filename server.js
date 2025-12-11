00cf222b5810c99026ad6e17226e1b6ae345a14a
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-12-11 13:10:31 +0100

    yes understanding cors error croos origin sharing ?

--- server.js
+++ server.js
@@ -16,3 +16,7 @@ app.listen(port,"0.0.0.0" ,()=>console.log(`server running on ${port}`))
 
 
 
+// app.use(cors({
+//     origin: "allowed port eg locall host 3000 ",
+//     credentials: true allow use of header 
+// }))
