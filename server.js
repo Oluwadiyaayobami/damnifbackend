bddc6f769024cd4cf7f2609f8a65c469d2dbcb20
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-12-11 13:03:33 +0100

    confirming endpoint

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const cors = require('cors');
 const databaseconnection = require('./database/db.js')
 const app = express()
 const routers = require('./routes/applicationrouthes.js')
 
-
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth',routers)
 const port = process.env.PORT
-app.listen(port, ()=>console.log(`server running on ${port}`))
+app.listen(port,"0.0.0.0" ,()=>console.log(`server running on ${port}`))
 
 
 
